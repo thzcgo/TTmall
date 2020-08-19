b543b997779e533173f50a2ef3c23696ae257db6
thzcgo <780417172@qq.com>
2020-08-19 12:33:50 +0800

    检索服务注册nacos

---
+++ ttmall-search/src/main/java/com/thzc/ttmall/search/TtmallSearchApplication.java
@@ -0,0 +1,13 @@
+package com.thzc.ttmall.search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TtmallSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TtmallSearchApplication.class, args);
+	}
+
+}
---
+++ ttmall-search/src/test/java/com/thzc/ttmall/search/TtmallSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thzc.ttmall.search;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TtmallSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
